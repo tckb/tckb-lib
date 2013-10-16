c7383358f9c9a47bfee58b2af44f4c7fc6318152
Chandra Tungathurthi <tckb4.504@gmail.com>
2013-10-16 18:43:50 +0200

    code - base clean up, removed useless files
    fixed issues with git; created jaudio branch

--- src/main/java/com/tckb/samples/AudioUIDemo.java
+++ src/main/java/com/tckb/samples/AudioUIDemo.java
@@ -2,7 +2,7 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.tckb.sandbox;
+package com.tckb.samples;
 
 import com.tckb.audio.part.Label;
 import com.tckb.audio.ui.AudioUI;
--- src/main/java/com/tckb/samples/UITest.java
+++ src/main/java/com/tckb/samples/UITest.java
@@ -2,7 +2,7 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.tckb.sandbox;
+package com.tckb.samples;
 
 import com.tckb.audio.ui.AudioUI;
 import com.tckb.audio.ui.display.AudioDisplay;
